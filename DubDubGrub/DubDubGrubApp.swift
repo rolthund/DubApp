8c4c07e8bfef31b7cfb02d30d8dea40ccd4fa436
rolthund <vladfmail@gmail.com>
2022-12-25 20:49:59 -0800

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Vladyslav Fesenko on 12/25/22.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
